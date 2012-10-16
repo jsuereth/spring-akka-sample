d0e416349d89c3a692fd88ba1a7b90a0fd6e7b13
Josh Suereth <joshua.suereth@gmail.com>
2012-10-16 10:56:17 -0400

    More fun for nilanjan.

--- src/main/java/org/springframework/samples/travel/search/AkkaSearchBean.scala
+++ src/main/java/org/springframework/samples/travel/search/AkkaSearchBean.scala
@@ -3,9 +3,16 @@ package search
 
 import org.springframework.stereotype.Service
 import javax.inject.Inject
+import javax.persistence.{EntityManager, PersistenceContext}
+
+import akka.actor._
 
 @Service
-class AkkaSearchBean extends SearchService  {
+class AkkaSearchBean(@PersistenceContext em: EntityManager) extends SearchService  {
+  
+  val system =  ActorSystem("search-service")
+  
+  
   override def findHotels(criteria: SearchCriteria): java.util.List[Hotel] = {
     println("ZOMG SEARCH: " + criteria)
     null

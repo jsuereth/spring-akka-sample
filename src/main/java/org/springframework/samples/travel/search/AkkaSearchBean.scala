ea4efe664fee63b92d217b2014b59c0ee35247f3
Josh Suereth <joshua.suereth@gmail.com>
2012-10-16 11:15:47 -0400

    Things now instantiate, even if ugly.

--- src/main/java/org/springframework/samples/travel/search/AkkaSearchBean.scala
+++ src/main/java/org/springframework/samples/travel/search/AkkaSearchBean.scala
@@ -2,20 +2,33 @@ package org.springframework.samples.travel
 package search
 
 import org.springframework.stereotype.Service
-import javax.inject.Inject
+import javax.inject.{Inject,Singleton}
 import javax.persistence.{EntityManager, PersistenceContext}
-
 import akka.actor._
+import scala.collection.JavaConverters._
+import javax.annotation.PostConstruct
 
 @Service
-class AkkaSearchBean(@PersistenceContext em: EntityManager) extends SearchService  {
+@Singleton
+class AkkaSearchBean extends SearchService  {
+  
+  @(PersistenceContext @annotation.target.setter)
+  var em: EntityManager = null
   
   val system =  ActorSystem("search-service")
   
+  @PostConstruct
+  def makeSearchActor = {
+    // Startup....
+    val hotels = em.createQuery("select h from Hotel h").getResultList.asInstanceOf[java.util.List[Hotel]].asScala
+    hotels foreach em.detach
+    hotels foreach println
+    // Now feed data into Akka Search service.
+    null
+  }
   
   override def findHotels(criteria: SearchCriteria): java.util.List[Hotel] = {
     println("ZOMG SEARCH: " + criteria)
     null
   }
-  
 }
